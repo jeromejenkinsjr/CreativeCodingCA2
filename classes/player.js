c087483c767c831468e7357f1264dd7992fef516
Jeremi Olczak <n00230483@iadt.ie>
2025-03-17 00:11:12 +0000

    Added weight detection system

--- classes/player.js
+++ classes/player.js
@@ -3,32 +3,34 @@ class Player {
 		this.size = 30;
 		this.x = width / 2;
 		this.y = height / 2;
+		this.weight = 3;
 	}
 
 	update() {
+		let effectiveSpeed = playerSpeed / this.weight;
 		playerVelY = 0;
 
 		if (
 			keyIsDown(LEFT_ARROW) &&
 			this.x - this.size / 2 > width / 2 - laneWidth / 2
 		) {
-			this.x -= playerSpeed;
+			this.x -= effectiveSpeed;
 		}
 		if (
 			keyIsDown(RIGHT_ARROW) &&
 			this.x + this.size / 2 < width / 2 + laneWidth / 2
 		) {
-			this.x += playerSpeed;
+			this.x += effectiveSpeed;
 		}
 		if (keyIsDown(UP_ARROW)) {
-			playerVelY = playerSpeed;
+			playerVelY = effectiveSpeed;
 		}
 		if (keyIsDown(DOWN_ARROW)) {
-			playerVelY = -playerSpeed;
+			playerVelY = -effectiveSpeed;
 		}
 	}
 
-    display() {
+	display() {
 		fill(255, 0, 0);
 		noStroke();
 		rectMode(CENTER);
