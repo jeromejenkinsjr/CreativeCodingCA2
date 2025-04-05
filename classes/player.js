31aea8614fd890e8e92c09b78476cea5ac5c5555
Jeremi Olczak <n00230483@iadt.ie>
2025-04-05 18:10:36 +0100

    Adjusting position of arc on user to represent where the player is facing

--- classes/player.js
+++ classes/player.js
@@ -11,16 +11,10 @@ class Player {
 		let effectiveSpeed = playerSpeed / this.weight;
 		scrollSpeed = 0;
 
-		if (
-			keyIsDown(65) &&
-			this.x - this.size / 2 > width / 2 - laneWidth / 2
-		) {
+		if (keyIsDown(65) && this.x - this.size / 2 > width / 2 - laneWidth / 2) {
 			this.x -= effectiveSpeed;
 		}
-		if (
-			keyIsDown(68) &&
-			this.x + this.size / 2 < width / 2 + laneWidth / 2
-		) {
+		if (keyIsDown(68) && this.x + this.size / 2 < width / 2 + laneWidth / 2) {
 			this.x += effectiveSpeed;
 		}
 		if (keyIsDown(87)) {
@@ -40,7 +34,7 @@ class Player {
 		push();
 		translate(this.x, this.y);
 		rotate(this.rotation);
-		
+
 		fill(255, 0, 0);
 		noStroke();
 		rectMode(CENTER);
@@ -48,7 +42,7 @@ class Player {
 
 		fill(0);
 		noStroke();
-		arc(0, -this.size/2, this.size/2, this.size/4, PI, TWO_PI);
+		arc(10, 0, this.size, this.size, -QUARTER_PI, QUARTER_PI);
 		pop();
 	}
 
@@ -56,14 +50,15 @@ class Player {
 		let d = dist(this.x, this.y, emp.position.x, emp.position.y);
 		let strength = map(d, 0, emp.radius, 1, 0);
 		strength = constrain(strength, 0, 1);
-	
+
 		this.weight = 3 + 2 * strength;
 		this.rotation += random(-0.05, 0.05) * strength;
-	
-		let push = p5.Vector.sub(createVector(this.x, this.y), emp.position).normalize().mult(strength * 2);
+
+		let push = p5.Vector.sub(createVector(this.x, this.y), emp.position)
+			.normalize()
+			.mult(strength * 2);
 
 		this.x += push.x;
 		this.y += push.y;
 	}
-	
 }
